42834ab2dcd155a28da664c62fb0a0701699e1e9
Jhon Ore Sulca <jefe.iot@navitrack.com.pe>
2026-06-16 15:39:03 -0500

    version2

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include <ArduinoOTA.h>
 
 // VERSION — cambia este numero para cada release
-String VERSION_LOCAL = "1.0.1";
+String VERSION_LOCAL = "1.0.2";
 
 const char* ssid     = "NAVITRACK -2.4G";
 const char* password = "N@V1TR4CK";
-
+#define LED 48
 void setup() {
     Serial.begin(115200);
     delay(1000);
@@ -20,9 +20,17 @@ void setup() {
 
     ArduinoOTA.begin();
     Serial.println("OTA listo");
+    /************************************************** */
+
+    pinMode(LED,OUTPUT):
 }
 
 void loop() {
     ArduinoOTA.handle();
+    digitalWrite(LED,1);
+    delay(100);
+    digitalWrite(LED,0);
+    delay(100);
+    
     // tu codigo aqui
 }
